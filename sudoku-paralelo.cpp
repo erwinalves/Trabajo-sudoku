339d8de068edeed55db77b0f9983728aa0b7b1e9
erwinalves <erwinalbo@hotmail.com>
2018-09-27 23:35:45 -0300

    arreglando detalles

--- sudoku-paralelo.cpp
+++ sudoku-paralelo.cpp
@@ -336,16 +336,16 @@ void llenarListasMatriz(int valor, int aux[][9]){
 }
 
 int main(int argc, char* argv[]){
-
+    unsigned t0, t1;
 	//Para ingresar los datos, se ingresa por ejemplo: ./ejecutable "[0;1;4][0;3;8][1;0;5][2;1;7][2;3;4][2;4;6][3;0;6][3;1;9][3;2;8][3;3;5][3;4;3][3;5;7][3;6;2][3;8;4][4;1;3][4;2;5][4;4;2][5;5;4][5;7;9][5;8;3][6;4;7][6;6;3][7;0;7][7;1;8][7;3;1][7;7;6][8;3;3][8;4;9][8;6;7][8;7;4][8;8;8]"
-
+    t0=clock();
     int tamano, procesador;
   	MPI_Init(&argc,&argv);
   	MPI_Comm_size(MPI_COMM_WORLD, &tamano); 
     MPI_Comm_rank(MPI_COMM_WORLD, &procesador);
 
     int resultado[9][9];
-    unsigned t0, t1;
+    
 
 
     Lista p;
@@ -435,14 +435,6 @@ int main(int argc, char* argv[]){
             i=largoArgumento;
         }
     }
-
-    if(comprobar && procesador==0){
-        cout<<"Guardados"<<endl;
-    }
-    else{
-        cout<<"Error de formato"<<endl;
-    }
-    
     
     int j=0, cont=0;
     while(j<largoDetalle){
@@ -461,9 +453,6 @@ int main(int argc, char* argv[]){
             j=j+3;
         }
     }
-
-  
-
 
     int indice;
     for(int i=0;i<9;i++){
@@ -492,32 +481,29 @@ int main(int argc, char* argv[]){
     }
     if(procesador ==0){
         ofstream fs("sudoku.csv");
-    for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
-            if(matriz[i][j]->esFijo==true){
-                if(j==8){
-                    fs<<matriz[i][j]->numero;
-                }
-                else{
-			        fs<<matriz[i][j]->numero<<";";
+            for(int i=0;i<9;i++){
+		        for(int j=0;j<9;j++){
+                    if(matriz[i][j]->esFijo==true){
+                        if(j==8){
+                        fs<<matriz[i][j]->numero;
+                    }
+                    else{
+			            fs<<matriz[i][j]->numero<<";";
+                    }
                 }
             }
+            fs<<endl;
         }
-        fs<<endl;
+        fs.close();
     }
-    fs.close();
-    
-
-
-
-    cout<<"Se ha imprimido en archivo sudoku.csv"<<endl;
+    if(procesador==0){
+        cout<<"Se ha imprimido en archivo sudoku.csv"<<endl;
+        
+        t1 = clock();
+        double time = (double(t1-t0)/CLOCKS_PER_SEC);
+        cout << "Execution Time: " << time << endl;
+    }
     MPI_Finalize();
 	return 0;
 }
-t0=clock();
-// Code to execute
-t1 = clock();
- 
-double time = (double(t1-t0)/CLOCKS_PER_SEC);
-cout << "Execution Time: " << time << endl;
-}
+
